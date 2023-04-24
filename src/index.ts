064368a9ff8795b224c22b771a7193e342022e36
Elliot Lintz <e.lintz@numendo.com>
2023-04-24 18:08:28 +0200

    :tada: reproduction

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import {TerminauxListeResultsVue} from "./terminaux-liste-results.vue";
+
+const test: string = 5;
+
+TerminauxListeResultsVue.init();
---
+++ src/terminaux-liste-results.vue.ts
@@ -0,0 +1,5 @@
+export class TerminauxListeResultsVue {
+
+  public static init() {
+  }
+}
